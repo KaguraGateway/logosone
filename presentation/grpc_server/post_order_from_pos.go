4688179273034371b4b79ae69a07313866e42e87
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-09-22 15:01:02 +0900

    feat: gRPCも実装

---
+++ presentation/grpc_server/post_order_from_pos.go
@@ -0,0 +1,45 @@
+package grpcserver
+
+import (
+	"context"
+
+	"connectrpc.com/connect"
+	"github.com/Code-Hex/synchro"
+	"github.com/Code-Hex/synchro/tz"
+	"github.com/KaguraGateway/cafelogos-grpc/pkg/common"
+	"github.com/KaguraGateway/cafelogos-grpc/pkg/orderlink"
+	"github.com/KaguraGateway/cafelogos-orderlink-backend/application"
+	"github.com/KaguraGateway/cafelogos-orderlink-backend/domain/model/order"
+	"github.com/samber/do"
+	"github.com/samber/lo"
+)
+
+func (r *GrpcServer) PostOrder(ctx context.Context, req *connect.Request[orderlink.PostOrderInput]) (*connect.Response[common.Empty], error) {
+	usecase := do.MustInvoke[application.PostOrderFromPos](r.i)
+	msg := req.Msg
+
+	orderAt, err := synchro.ParseISO[tz.UTC](msg.OrderAt)
+	if err != nil {
+		return nil, err
+	}
+
+	input := &application.PostOrderInput{
+		OrderId: msg.OrderId,
+		OrderAt: orderAt,
+		OrderItems: lo.Map(msg.Items, func(item *orderlink.PostOrderItemInput, _ int) application.PostOrderItemInput {
+			return application.PostOrderItemInput{
+				ProductId:    item.ProductId,
+				CoffeeBrewId: &item.CoffeeBrewId,
+				Quantity:     uint(item.Quantity),
+			}
+		}),
+		OrderType:  order.OrderType(msg.Type),
+		TicketId:   msg.TicketId,
+		TicketAddr: msg.TicketAddr,
+		SeatName:   &msg.SeatName,
+	}
+	if err := usecase.Execute(ctx, input); err != nil {
+		return nil, err
+	}
+	return connect.NewResponse(&common.Empty{}), nil
+}

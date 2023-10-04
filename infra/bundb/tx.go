8fa4f88768d41b37254011b0a985d0693f2eba3d
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-10-04 22:03:49 +0900

    feat: repository.Txを実装

---
+++ infra/bundb/tx.go
@@ -0,0 +1,26 @@
+package bundb
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/KaguraGateway/cafelogos-pos-backend/domain/repository"
+	"github.com/samber/do"
+	"github.com/uptrace/bun"
+)
+
+type txRepositoryDb struct {
+	db *bun.DB
+}
+
+func NewTxRepositoryDb(i *do.Injector) (repository.TxRepository, error) {
+	return &txRepositoryDb{
+		db: do.MustInvoke[*bun.DB](i),
+	}, nil
+}
+
+func (r *txRepositoryDb) Transaction(ctx context.Context, fn func(ctx context.Context, tx interface{}) error) error {
+	return r.db.RunInTx(ctx, &sql.TxOptions{}, func(ctx context.Context, tx bun.Tx) error {
+		return fn(ctx, tx)
+	})
+}

8213df84e91f0ad602c60d41af9bfd02164be5e8
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-10-04 23:29:13 +0900

    feat: gRPC/UpdateOrderPaymentを実装

--- presentation/grpc_server/update_order_payment.go
+++ presentation/grpc_server/update_order_payment.go
@@ -4,8 +4,23 @@ import (
 	"connectrpc.com/connect"
 	"context"
 	"github.com/KaguraGateway/cafelogos-grpc/pkg/pos"
+	"github.com/KaguraGateway/cafelogos-pos-backend/application"
+	"github.com/samber/do"
 )
 
 func (s *GrpcServer) UpdateOrderPayment(ctx context.Context, req *connect.Request[pos.UpdateOrderPaymentRequest]) (*connect.Response[pos.OrderPaymentResponse], error) {
-	return nil, connect.NewError(connect.CodeUnimplemented, nil)
+	usecase := do.MustInvoke[application.SaveOrderPayment](s.i)
+	param, err := ToOrderPaymentParam(req.Msg.Payment)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInternal, err)
+	}
+	payment, err := usecase.Execute(ctx, req.Msg.OrderId, *param)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInternal, err)
+	}
+
+	return connect.NewResponse(&pos.OrderPaymentResponse{
+		IsOk:    true,
+		Payment: ToProtoOrderPayment(payment),
+	}), nil
 }

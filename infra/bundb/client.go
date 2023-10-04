5712946b205ad825dbf2ca51bda2ba051d1b086d
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-10-04 16:15:46 +0900

    feat: repository.Clientの実装

---
+++ infra/bundb/client.go
@@ -0,0 +1,29 @@
+package bundb
+
+import (
+	"context"
+	"github.com/KaguraGateway/cafelogos-pos-backend/domain/model"
+	"github.com/KaguraGateway/cafelogos-pos-backend/domain/repository"
+	"github.com/KaguraGateway/cafelogos-pos-backend/infra/bundb/dao"
+	"github.com/samber/do"
+	"github.com/uptrace/bun"
+)
+
+type clientDb struct {
+	db *bun.DB
+}
+
+func NewClientDb(i *do.Injector) (repository.ClientRepository, error) {
+	return &clientDb{db: do.MustInvoke[*bun.DB](i)}, nil
+}
+
+func (i *clientDb) Save(ctx context.Context, client *model.Client) error {
+	daoClient := &dao.Client{
+		ID:   client.GetId(),
+		Name: client.GetName(),
+	}
+	if _, err := i.db.NewInsert().Model(daoClient).On("CONFLICT (id) DO UPDATE").Set("name = EXCLUDED.name").Exec(ctx); err != nil {
+		return err
+	}
+	return nil
+}

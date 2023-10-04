01261a6800d519ff891cd088eb6c83e09464508c
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-10-04 22:28:17 +0900

    feat: gRPC/DeleteOrderを実装

--- presentation/grpc_server/delete_order.go
+++ presentation/grpc_server/delete_order.go
@@ -5,8 +5,14 @@ import (
 	"context"
 	"github.com/KaguraGateway/cafelogos-grpc/pkg/common"
 	"github.com/KaguraGateway/cafelogos-grpc/pkg/pos"
+	"github.com/KaguraGateway/cafelogos-pos-backend/application"
+	"github.com/samber/do"
 )
 
 func (s *GrpcServer) DeleteOrder(ctx context.Context, req *connect.Request[pos.DeleteOrderRequest]) (*connect.Response[common.Empty], error) {
-	return nil, connect.NewError(connect.CodeUnimplemented, nil)
+	usecase := do.MustInvoke[application.DeleteOrder](s.i)
+	if err := usecase.Execute(ctx, req.Msg.OrderId); err != nil {
+		return nil, connect.NewError(connect.CodeInternal, err)
+	}
+	return connect.NewResponse(&common.Empty{}), nil
 }

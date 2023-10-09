ad268a0a892bff7eda715b20116c9ce03cd68bb4
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-10-10 01:27:12 +0900

    fix: 力技でlayout.tsxを修正

--- app/(handy)/layout.tsx
+++ app/(handy)/layout.tsx
@@ -1,11 +1,14 @@
 'use client';
-
-import React from "react";
+import { Box } from '@chakra-ui/react';
+import { Header } from '@/ui/Header';
 
 export default function HandyLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Header />
+      <Box mt="60px">
         {children}
+      </Box>
     </>
   );
 }
--- app/(handy)/page.tsx
+++ app/(handy)/page.tsx
@@ -1,8 +1,12 @@
 import Home from './(header)/home/page';
+import { Box } from '@chakra-ui/react';
 
 export default function Handy() {
   return (
-    <Home />
+    <Box p="1.5rem">
+      <Home  />
+    </Box>
+    
   );
 }
 

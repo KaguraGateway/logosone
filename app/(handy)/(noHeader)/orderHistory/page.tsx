f676fff5f5f030e5e0c557c58a19f803588771af
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-10-10 01:06:52 +0900

    feat: 注文履歴ページを追加

--- app/(handy)/(noHeader)/orderHistory/page.tsx
+++ app/(handy)/(noHeader)/orderHistory/page.tsx
@@ -4,16 +4,15 @@ import { Button, Center, Flex, Text } from "@chakra-ui/react";
 import Link from 'next/link';
 import ProductInfoCard from "../_components/ProductInfoCard";
 import { HiCheckCircle } from "react-icons/hi";
-import { IoArrowBackOutline } from "react-icons/io5";
-import ErrorModal from "./_components/ErrorModal";
+import { IoArrowBackOutline, IoClipboard } from "react-icons/io5";
 
 export default function orderCheck() {
-    const { isOpen: isOpenErrorModal, onOpen: onOpenErrorModal, onClose: onCloseErrorModal } = useDisclosure();
     return (
     <>  
         <Center>
             <Text fontSize="2xl" fontWeight="semibold" color="gray.600"  p={4} >
-                注文確認
+                テーブル1　注文履歴
+                {/* なんかいい感じに値渡してくれ */}
             </Text>
         </Center>
         <Flex flexDir="column" padding={1} width="100%" gap={4} paddingX={4}>
@@ -22,25 +21,18 @@ export default function orderCheck() {
             <ProductInfoCard name="レモネード" quantity={1} />
         </Flex>
         <Flex flexDir="row"  position="fixed" width="100vw" alignItems="center" bottom="0" left="0" right="0" minHeight="70px" bg="white" paddingTop={3} paddingBottom={5} borderTop="2px" borderColor="gray.300" boxShadow="base" paddingX={4}>
-            <Button flex={1} size="lg" colorScheme="red" leftIcon={<IoArrowBackOutline />}  marginRight={4}
+            <Button flex={1} size="lg" colorScheme="gray" leftIcon={<IoArrowBackOutline />}  marginRight={4} bg={"gray.300"}
             as={Link} href="/orderEntry">
                 戻る
             </Button>
-            <Button flex={3} size="lg" colorScheme="green" leftIcon={<HiCheckCircle />} 
-            onClick={onOpenErrorModal}
-            // as={Link} href="/waiter"
+            <Button flex={3} size="lg" colorScheme="blue" leftIcon={<IoClipboard />} 
+            as={Link} href="/orderEntry"
             >
-                注文送信
+                追加注文
             </Button>
         </Flex>
 
-        <ErrorModal
-            isOpen={isOpenErrorModal}
-            onClose={onCloseErrorModal}
-            onOpen={onOpenErrorModal}
-            errorTitle="注文送信エラー"
-            errorMessage="注文を送信できませんでした。"
-        />
+        
     </>
   );
 }

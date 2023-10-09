665a57ae66649673bce1ed470cfa141281ced257
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-10-10 01:32:50 +0900

    fix: リンクを修正

--- app/(handy)/(noHeader)/staffSelection/page.tsx
+++ app/(handy)/(noHeader)/staffSelection/page.tsx
@@ -11,12 +11,12 @@ export default function staffSelection() {
                         担当者選択
                     </Text>
                 </Center>
-            <Button size="lg" colorScheme="teal" h="100px" leftIcon={<IoPeople />} as={Link} href="/orderEntry" >
+            <Button size="lg" colorScheme="teal" h="100px" leftIcon={<IoPeople />} as={Link} href="/waiter" >
                 ロゴスメンバー
             </Button>
             <Button size="lg" 
                 bg="white"  color="gray.600" whiteSpace="unset" width="100%" paddingY={8} boxShadow="base" border="1px" borderColor="gray.300"  h="100px" leftIcon={<IoPeople />} 
-                as={Link} href="/orderEntry" >
+                as={Link} href="/waiter" >
                 トレーニングモード
                 <br/>
                 （注文は送信されません）

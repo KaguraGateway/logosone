0001dffe34df98c23a2fda6b4f9ac479a357f2ae
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-10-09 21:09:11 +0900

    feat: いい感じに数字を制御する変数を追加

--- app/(handy)/(noHeader)/orderEntry/_components/QuantitySelector.tsx
+++ app/(handy)/(noHeader)/orderEntry/_components/QuantitySelector.tsx
@@ -1,22 +1,38 @@
-import React from 'react';
 import { Button, Flex, Spacer } from '@chakra-ui/react';
 import { DeleteIcon, MinusIcon, AddIcon } from '@chakra-ui/icons';
 
-export default function QuantitySelector() {
+type Props = {
+    quantity: number;
+    onQuantityChange: (newQuantity: number) => void;
+}
+
+export default function QuantitySelector(props: Props) {
+    const handleDeleteClick = () => {
+        props.onQuantityChange(0);
+    };
+
+    const handleMinusClick = () => {
+        if (props.quantity > 0) {
+            props.onQuantityChange(props.quantity - 1);
+        }
+    };
+
+    const handleAddClick = () => {
+        props.onQuantityChange(props.quantity + 1);
+    };
+
     return (
-        <>
         <Flex flexDir="row" gap={3} m={2}>
-                    <Button size="lg" bg="red" color="white">
-                        <DeleteIcon />
-                    </Button>
-                    <Spacer />
-                    <Button size="lg" bg="gray" color="white" paddingX={10}>
-                        <MinusIcon />
-                    </Button>
-                    <Button size="lg" bg="gray" color="white" paddingX={10}>
-                        <AddIcon />
-                    </Button>
+            <Button size="lg" bg="red" color="white" onClick={handleDeleteClick}>
+                <DeleteIcon />
+            </Button>
+            <Spacer />
+            <Button size="lg" bg="gray" color="white" paddingX={10} onClick={handleMinusClick}>
+                <MinusIcon />
+            </Button>
+            <Button size="lg" bg="gray" color="white" paddingX={10} onClick={handleAddClick}>
+                <AddIcon />
+            </Button>
         </Flex>
-        </>
     );
 }

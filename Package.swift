a6512e8317f7f17beaa4b58a9ba8974cfe0e384c
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-10-11 09:08:18 +0900

    deps setting

--- Package.swift
+++ Package.swift
@@ -11,11 +11,20 @@ let package = Package(
             name: "cafelogos-grpc",
             targets: ["cafelogos-grpc"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
+        .package(url: "https://github.com/connectrpc/connect-swift.git", from: "0.8.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "cafelogos-grpc"),
+            name: "cafelogos-grpc",
+            dependencies: [
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                .product(name: "Connect", package: "connect-swift")
+            ]
+        ),
         .testTarget(
             name: "cafelogos-grpcTests",
             dependencies: ["cafelogos-grpc"]),

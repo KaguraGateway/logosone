f4c0d1e21477c6176a9b2cb9fdc33523ddf3d665
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-10-15 03:24:26 +0900

    fix: 同じコーヒで淹れ方が違うと片方保存が無視されるのを修正するため、CoffeeBrewIdもpkeyに追加

--- infra/bundb/dao/order_item.go
+++ infra/bundb/dao/order_item.go
@@ -1,12 +1,12 @@
 package dao
 
 type OrderItem struct {
-	OrderID      string   `bun:",pk"`
-	Order        *Order   `bun:"rel:belongs-to,join:order_id=id"`
-	ProductID    string   `bun:",pk"`
-	Product      *Product `bun:"rel:belongs-to,join:product_id=id"`
-	Quantity     uint64   `bun:",notnull"`
-	Amount       uint64   `bun:",notnull"`
-	CoffeeBrewID string
+	OrderID      string             `bun:",pk"`
+	Order        *Order             `bun:"rel:belongs-to,join:order_id=id"`
+	ProductID    string             `bun:",pk"`
+	Product      *Product           `bun:"rel:belongs-to,join:product_id=id"`
+	Quantity     uint64             `bun:",notnull"`
+	Amount       uint64             `bun:",notnull"`
+	CoffeeBrewID string             `bun:",pk"`
 	CoffeeBrew   *ProductCoffeeBrew `bun:"rel:belongs-to,join:coffee_brew_id=id"`
 }

ad85cd8a7df26965dfaf33044a233792fd50eb82
Yukinaga OISHI <c1223781@st.kanazawa-it.ac.jp>
2024-10-15 16:01:37 +0900

    feature: get seat list and name

--- app/(handy)/(header)/waiter/_components/TicketSelectButton.tsx
+++ app/(handy)/(header)/waiter/_components/TicketSelectButton.tsx
@@ -1,8 +1,8 @@
 import { Button } from "@chakra-ui/react";
 
 type Props = {
-    id: number;
-    number: number;
+    id: string;
+    title: string;
     onClick?: () => void;
 
 }
@@ -11,7 +11,7 @@ export default function TicketSelectButton(props: Props) {
   return (
   <>
     <Button size="lg" colorScheme="gray" h={16} bg="gray.50" color="gray.600" shadow="base" fontSize="xl" onClick={props.onClick}>
-    {props.number}
+    {props.title}
     </Button>
   </>
   );

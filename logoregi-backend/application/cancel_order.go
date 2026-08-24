ac48fd72e7989012ef98614bf29b320df2dac41e
MasudaKeitoLB <keito.masuda2005@gmail.com>
2026-08-24 19:44:09 +0900

    gRPC連携を諦める

--- logoregi-backend/application/cancel_order.go
+++ logoregi-backend/application/cancel_order.go
@@ -3,18 +3,10 @@ package application
 import (
 	"context"
 	"fmt"
-	"log"
-
-	"connectrpc.com/connect"
 
 	"github.com/KaguraGateway/logosone/logoregi-backend/domain/model"
 	"github.com/KaguraGateway/logosone/logoregi-backend/domain/repository"
 	"github.com/samber/do"
-
-	"github.com/KaguraGateway/cafelogos-grpc/pkg/orderlink"
-	"github.com/KaguraGateway/cafelogos-grpc/pkg/orderlink/orderlinkconnect"
-	"github.com/KaguraGateway/cafelogos-grpc/pkg/ticket"
-	"github.com/KaguraGateway/cafelogos-grpc/pkg/ticket/ticketconnect"
 )
 
 type CancelOrder interface {
@@ -27,20 +19,15 @@ type cancelOrderUseCase struct {
 	productQS ProductQueryService
 	stockRepo repository.StockRepository
 	txRepo    repository.TxRepository
-
-	orderLinkClient orderlinkconnect.OrderLinkServiceClient
-	ticketClient    ticketconnect.TicketServiceClient
 }
 
 func NewCancelOrderUseCase(i *do.Injector) (CancelOrder, error) {
 	return &cancelOrderUseCase{
-		orderQS:         do.MustInvoke[OrderQueryService](i),
-		orderRepo:       do.MustInvoke[repository.OrderRepository](i),
-		productQS:       do.MustInvoke[ProductQueryService](i),
-		stockRepo:       do.MustInvoke[repository.StockRepository](i),
-		txRepo:          do.MustInvoke[repository.TxRepository](i),
-		orderLinkClient: do.MustInvoke[orderlinkconnect.OrderLinkServiceClient](i),
-		ticketClient:    do.MustInvoke[ticketconnect.TicketServiceClient](i),
+		orderQS:   do.MustInvoke[OrderQueryService](i),
+		orderRepo: do.MustInvoke[repository.OrderRepository](i),
+		productQS: do.MustInvoke[ProductQueryService](i),
+		stockRepo: do.MustInvoke[repository.StockRepository](i),
+		txRepo:    do.MustInvoke[repository.TxRepository](i),
 	}, nil
 }
 
@@ -57,45 +44,17 @@ func (uc *cancelOrderUseCase) Execute(ctx context.Context, orderId string) error
 		return nil
 	}
 
-	statusMap := make(map[string]int32)
-	olReq := connect.NewRequest(&orderlink.CancelOrderInput{
-		OrderID: orderId,
-	})
-	olResp, err := uc.orderLinkClient.CancelOrder(ctx, olReq)
-
-	if err == nil && olResp != nil && olResp.Msg != nil {
-		for _, item := range olResp.Msg.Items {
-			statusMap[item.ProductID] = item.Status
-		}
-	} else {
-		log.Printf("OrderLink cancel warning (maybe not sent to kitchen): %v", err)
-	}
-
 	err = uc.txRepo.Transaction(ctx, func(ctx context.Context, tx interface{}) error {
 		var updateStocks []*model.Stock
+
 		for _, item := range order.GetOrderItems() {
 			product, err := uc.productQS.FindById(ctx, item.GetProductId())
 			if err != nil {
 				return err
 			}
 			if product.Stock != nil {
-				shouldRestore := false
-
-				if product.ProductType == model.ProductType(model.Other) {
-					shouldRestore = true
-				} else if product.ProductType == model.ProductType(model.Coffee) {
-					if status, exists := statusMap[product.GetId()]; exists {
-						if status == 0 {
-							shouldRestore = true
-						}
-					} else {
-						shouldRestore = true
-					}
-				}
-				if shouldRestore {
-					product.Stock.Quantity += int32(item.Quantity)
-					updateStocks = append(updateStocks, product.Stock)
-				}
+				product.Stock.Quantity += int32(item.Quantity)
+				updateStocks = append(updateStocks, product.Stock)
 			}
 		}
 
@@ -114,14 +73,6 @@ func (uc *cancelOrderUseCase) Execute(ctx context.Context, orderId string) error
 
 	if err != nil {
 		return fmt.Errorf("transaction failed: %w", err)
-	}
-
-	ticketReq := connect.NewRequest(&ticket.RequestRevokeTicket{
-		ID: orderId,
-	})
-	_, err = uc.ticketClient.RevokeTicket(ctx, ticketReq)
-	if err != nil {
-		log.Printf("failed to revoke ticket (maybe no ticket): %v", err)
 	}
 
 	return nil
